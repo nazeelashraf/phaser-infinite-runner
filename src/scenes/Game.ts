2e2f6e48f9e956c10c3c7ec2170d64a243ab1888
Nazeel <nazeelash@gmail.com>
2022-09-11 21:23:34 +0530

    feat: added camera follow

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,8 +1,11 @@
 import Phaser, { Physics } from "phaser";
 import AnimationKeys from "~/consts/AnimationKeys";
 import SceneKeys from "~/consts/SceneKeys";
+import TextureKeys from "~/consts/TextureKeys";
 
 export default class Game extends Phaser.Scene {
+
+    private background!: Phaser.GameObjects.TileSprite
 
     constructor() {
         super(SceneKeys.Game)
@@ -17,8 +20,9 @@ export default class Game extends Phaser.Scene {
         const width = this.scale.width
         const height = this.scale.height
 
-        this.add.tileSprite(0, 0, width, height, 'background')
+        this.background = this.add.tileSprite(0, 0, width, height, TextureKeys.Background)
             .setOrigin(0)
+            .setScrollFactor(0, 0)
 
         const mouse = this.physics.add.sprite(
             width * 0.5,
@@ -40,5 +44,9 @@ export default class Game extends Phaser.Scene {
 
         this.cameras.main.startFollow(mouse)
         this.cameras.main.setBounds(0, 0, Number.MAX_SAFE_INTEGER, height)
+    }
+
+    update(time: number, delta: number): void {
+        this.background.setTilePosition(this.cameras.main.scrollX)
     }
 }
