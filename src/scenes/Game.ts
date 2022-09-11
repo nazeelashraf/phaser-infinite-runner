78a1b44f6f952bcfe984ae63bb03e72c45478bf6
Nazeel <nazeelash@gmail.com>
2022-09-11 23:22:15 +0530

    feat: added windows and bookcases, overlap logic implemented

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -7,13 +7,16 @@ export default class Game extends Phaser.Scene {
 
     private background!: Phaser.GameObjects.TileSprite
     private mouseHole!: Phaser.GameObjects.Image
+    private window1!: Phaser.GameObjects.Image
+    private window2!: Phaser.GameObjects.Image
+    private bookcase1!: Phaser.GameObjects.Image
+    private bookcase2!: Phaser.GameObjects.Image
 
+    private bookcases: Phaser.GameObjects.Image[] = []
+    private windows: Phaser.GameObjects.Image[] = []
+    
     constructor() {
         super(SceneKeys.Game)
-    }
-
-    preload() {
-        
     }
 
     create() {
@@ -30,6 +33,36 @@ export default class Game extends Phaser.Scene {
             501,
             TextureKeys.MouseHole
         )
+
+        this.window1 = this.add.image(
+            Phaser.Math.Between(900, 1300),
+            200,
+            TextureKeys.Window1
+        )
+
+        this.window2 = this.add.image(
+            Phaser.Math.Between(1600, 2000),
+            200,
+            TextureKeys.Window2
+        )
+
+        this.windows =[this.window1, this.window2]
+
+        this.bookcase1 = this.add.image(
+            Phaser.Math.Between(2200, 2700),
+            580,
+            TextureKeys.Bookcase1
+        )
+        .setOrigin(0.5, 1)
+
+        this.bookcase2 = this.add.image(
+            Phaser.Math.Between(2900, 3400),
+            580,
+            TextureKeys.Bookcase2
+        )
+        .setOrigin(0.5, 1)
+
+        this.bookcases = [this.bookcase1, this.bookcase2]
 
         const mouse = this.physics.add.sprite(
             width * 0.5,
@@ -55,6 +88,8 @@ export default class Game extends Phaser.Scene {
 
     update(time: number, delta: number): void {
         this.wrapMouseHole()
+        this.wrapWindows()
+        this.wrapBookcases()
         this.background.setTilePosition(this.cameras.main.scrollX)
     }
 
@@ -67,6 +102,76 @@ export default class Game extends Phaser.Scene {
                 rightEdge + 100,
                 rightEdge + 1000
             )
+        }
+    }
+
+    private wrapWindows() {
+        const scrollX = this.cameras.main.scrollX
+        const rightEdge = scrollX + this.scale.width
+
+        let width = this.window1.width * 2
+
+        if(this.window1.x + width < scrollX) {
+            this.window1.x = Phaser.Math.Between(
+                rightEdge + width,
+                rightEdge + width + 800
+            )
+
+            const overlap = this.bookcases.find(bookcase => {
+                return Math.abs(this.window1.x - bookcase.x) <= this.window1.width
+            })
+
+            this.window1.visible = !overlap
+        }
+
+        width = this.window2.width
+
+        if(this.window2.x + width < scrollX) {
+            this.window2.x = Phaser.Math.Between(
+                this.window1.x + width,
+                this.window1.x + width + 800
+            )
+
+            const overlap = this.bookcases.find(bookcase => {
+                return Math.abs(this.window2.x - bookcase.x) <= this.window2.width
+            })
+
+            this.window2.visible = !overlap
+        }
+    }
+
+    private wrapBookcases() {
+        const scrollX = this.cameras.main.scrollX
+        const rightEdge = scrollX + this.scale.width
+
+        let width = this.bookcase1.width * 2
+
+        if(this.bookcase1.x + width < scrollX) {
+            this.bookcase1.x = Phaser.Math.Between(
+                rightEdge + width,
+                rightEdge + width + 800
+            )
+
+            const overlap = this.windows.find(window => {
+                return Math.abs(this.bookcase1.x - window.x) <= this.bookcase1.width
+            })
+
+            this.bookcase1.visible = !overlap
+        }
+
+        width = this.bookcase2.width
+
+        if(this.bookcase2.x + width < scrollX) {
+            this.bookcase2.x = Phaser.Math.Between(
+                this.bookcase1.x + width,
+                this.bookcase1.x + width + 800
+            )
+
+            const overlap = this.windows.find(window => {
+                return Math.abs(this.bookcase2.x - window.x) <= this.bookcase2.width
+            })
+
+            this.bookcase2.visible = !overlap
         }
     }
 }
