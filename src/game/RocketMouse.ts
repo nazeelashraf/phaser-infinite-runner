70b10723b384b85027314c92779ddf8dc98cfbcf
Nazeel <nazeelash@gmail.com>
2022-09-13 23:19:58 +0530

    feat: added flying logic and flame, fly & fall animations

--- src/game/RocketMouse.ts
+++ src/game/RocketMouse.ts
@@ -4,25 +4,59 @@ import TextureKeys from "~/consts/TextureKeys"
 
 export default class RocketMouse extends Phaser.GameObjects.Container {
     
+    private flames: Phaser.GameObjects.Sprite
+    private cursors: Phaser.Types.Input.Keyboard.CursorKeys
+    private mouse: Phaser.GameObjects.Sprite
+
     constructor(scene: Phaser.Scene, x: number, y: number) {
         super(scene, x, y)
 
-        const mouse = scene.add.sprite(0, 0, TextureKeys.RocketMouse)
+        this.mouse = scene.add.sprite(0, 0, TextureKeys.RocketMouse)
             .setOrigin(0.5, 1)
             .play(AnimationKeys.RocketMouseRun)
 
-        const flames = scene.add.sprite(-63, -15, TextureKeys.RocketMouse)
+        this.flames = scene.add.sprite(-63, -15, TextureKeys.RocketMouse)
             .play(AnimationKeys.RocketFlamesOn)
+        this.enableJetpack(false)
 
-        this.add(flames)
+        this.add(this.flames)
 
-        this.add(mouse)
+        this.add(this.mouse)
 
         scene.physics.add.existing(this)
 
         const body = this.body as Phaser.Physics.Arcade.Body
-        body.setSize(mouse.width, mouse.height)
-        body.setOffset(mouse.width * -0.5, -mouse.height)
+        body.setSize(this.mouse.width, this.mouse.height)
+        body.setOffset(this.mouse.width * -0.5, -this.mouse.height)
+
+        // controls
+        this.cursors = scene.input.keyboard.createCursorKeys()
     }
+
+    enableJetpack(enabled: boolean) {
+        this.flames.setVisible(enabled)
+    }
+
+    preUpdate() {
+        const body = this.body as Phaser.Physics.Arcade.Body
+
+        if(this.cursors.space?.isDown) {
+            body.setAccelerationY(-600)
+            this.enableJetpack(true)
+            
+            this.mouse.play(AnimationKeys.RocketMouseFly, true)
+        } else {
+            body.setAccelerationY(0)
+            this.enableJetpack(false)
+        }
+
+        if(body.blocked.down) {
+            this.mouse.play(AnimationKeys.RocketMouseRun, true)
+        } else if (body.velocity.y > 0) {
+            this.mouse.play(AnimationKeys.RocketMouseFall, true)
+        }
+    }
+
+
 
 }
